9f07d54b156f9e3006ec90b53653f0ef80485bb3
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-03-03 18:36:17 -0500

    Task #67, existing users
    I create a system so that people can't register over existing users.  This still needs some stuff like preventing special characters and a character limit.

--- static/php/register.php
+++ static/php/register.php
@@ -10,11 +10,50 @@ $path = '../data/log.txt';
     // Open the file in write mode.
     $file = fopen($path, "a") or die("cant open");
 
-    // Declare a single string of both values.
-    $data = $_POST['username'] . ":" . $_POST['password'] . "\n";
+    // Get contents of the file.  Different from fopen.  Just read access.
+    $data = file_get_contents($path);
 
-    // Write to file.
-    fwrite($file, $data);
+    // Break into arrays based on users.
+    $users = explode("\n", $data);
+
+    // Declare a single string of both values.
+    $user = $_POST['username'];
+        
+    // Declare a Boolean to see if the user already exists.
+    $exists = False;
+
+    // Iterate over each entry to check if the name exists.
+    foreach($users as $line) {
+
+        // Break the user apart by user, password.  0 is user, 1 is password
+        $current_user = explode(":", $line)[0];
+        
+        // Does the username match the desired name?
+        if ($current_user == $user){
+
+            // If so, change to say that it exists.
+            $exists = True;
+        }
+
+    }
+    
+    // Simple Boolean.
+    if ($exists == False){
+     
+        $data = $user . ":" . $_POST['password'] . "\n";
+
+        // Write to file.
+        fwrite($file, $data);
+        
+        // There's probably a better way to do this.
+        echo "<script>alert('You have been registered.');</script>"; 
+        
+    }
+    
+    else{
+        
+        echo "<script>alert('User already exists.');</script>";    
+    }
 
     // Close the file.
     fclose($file);
