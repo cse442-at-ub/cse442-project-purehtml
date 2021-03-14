160c17ca9ff7227918e4ba09fe30e6c19a4141e5
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-03-13 19:26:53 -0500

    Task #110, storing results in JSON
    This stores results in a JSON when the user is logged in.  The data is protected so that it cannot be accessed by anyone save those with server permissions.

--- static/php/set_artist.php
+++ static/php/set_artist.php
@@ -3,6 +3,7 @@
 // Start the session
 session_start();
 ?>
+ 
 <?php
 
 // Set the working artist.
@@ -10,12 +11,32 @@ session_start();
 // A boolean check to make sure that the fields have values.
  if (isset($_POST['artist'])) {
 
-    $current_artist = $_POST['artist']; 
-    
+    $current_artist = $_POST['artist'];
+
     $_SESSION['search'] = $current_artist;
-    
+
+    $user = $_SESSION["username"];
+    if ($user != ""){
+        
+        // MAKE SURE THAT THE FILE HAS {} IN IT IF EMPTY, + CHMOD 777
+        $file = '../data/history.json';
+        $json = file_get_contents($file) or die('No Open!');
+        $dict = json_decode($json, true);
+
+        // Does the user exists?
+        if (array_key_exists($user, $dict) == False){
+                $dict[$user] = array();
+        }
+
+        $array = $dict[$user];
+        $array[] = $current_artist;
+        $dict[$user] = $array;
+
+        $new_json = json_encode($dict);
+        file_put_contents($file, $new_json);
+    }
+
  }
 ?>
 <script> location.href = '../result.php'; </script>
 </html>
-
