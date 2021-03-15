f55fed0d3c4fda33858a5561d2f7b4e5701e4020
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-03-15 00:31:08 -0400

    Task #115, incorrect artist
    This sets the top match as opposed to searching for the correct artist.  Since we don't have an algorithm yet, this is a temporary solution.

--- static/php/set_artist.php
+++ static/php/set_artist.php
@@ -5,16 +5,27 @@ session_start();
 ?>
 <?php
 
+include '../../spotify/get_songs.php';
 // Set the working artist.
 
 // A boolean check to make sure that the fields have values.
  if (isset($_POST['artist'])) {
 
-    $current_artist = $_POST['artist']; 
-    
-    $_SESSION['search'] = $current_artist;
-    
+    $current_artist = $_POST['artist'];
+    $credentials = getCredentials();
+
+    $client_id = $credentials[0];
+    $client_secret = $credentials[1];
+    $token = get_authenicated($client_id, $client_secret);
+
+    $tracks = get_top_artists($current_artist, $token);
+    $top_match = $tracks[0]['artists'][0]['name'] or "Artist Not Found";
+
+    $_SESSION['search'] = $top_match;
+
+    $_SESSION['artist_tracks'] = $tracks;
  }
+
 ?>
 <script> location.href = '../result.php'; </script>
 </html>
