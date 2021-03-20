388c9b5a73dc67ba2ece9b9b6d86477be1dea8d0
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-03-20 03:12:12 -0400

    Task #85, search constraints
    Adding more to results page to reflect search constraints + some miscellaneous housekeeping.

--- static/result.php
+++ static/result.php
@@ -33,76 +33,85 @@ font-family: "Helvetica", "Arial", sans-serif;
 <?php include "header.php"; include "../spotify/get_songs.php";?>
 
 <body style="background-color: #77d94c">
-            <div class="row">
+        <div class="row">
 
-                    <!-- <div> elements are stacked vertically so this will appear under the last <div>. -->
-                    <div class="column" style="min-width: 400px; margin: 0 auto;">
-
-
-                            <!-- '<form action = "result.php">' redirects to result.html upon completion of the action.  In this case, clicking a button. -->
-
-                            <form action = "php/get_random.php">
-
-                                    <center>
-
-                                            <!-- Using our special .button class to make the button look a precise way, plus also text centering.  -->
-                                            <button class="button button1"><b>Random Song</b></button><br>
-
-                                    </center>
+                <!-- <div> elements are stacked vertically so this will appear under the last <div>. -->
+                <div class="column" style="min-width: 400px; margin: 0 auto;">
 
 
-                            </form>
+                        <!-- '<form action = "result.php">' redirects to result.html upon completion of the action.  In this case, clicking a button. -->
 
-                    </div>
+                        <form action = "php/get_random.php">
 
-                    <div class="column" style="min-width: 400px; margin: 0 auto;">
-                            <center>
-                                    <?php
+                                <center>
 
-                                            if ($_SESSION["username"] == ""){
-                                                    print "<h1>You searched: </h1> <p>{$_SESSION["search"]}</p>";
-                                            }
+                                        <!-- Using our special .button class to make the button look a precise way, plus also text centering.  -->
+                                        <button class="button button1"><b>Random Song</b></button><br>
 
-                                            else{
-                                                    print "<h1>{$_SESSION["username"]}, you searched: </h1> <h2>{$_SESSION["search"]}</h2>";
-                                            }
-                                    ?>
+                                </center>
 
-                                    <?php
-                                            $id = get_artist_id($_SESSION['search'], $_SESSION['artist_tracks']) or die("hi");
-                                            $url = get_artist_image($id[0], $_SESSION['token'])["url"];
-                                            if ($url != ""){
-                                                    print "<img src='{$url}' width = '200' height = '200' style = 'border: 5px solid black;'></img>";
-                                                    }
-                                    ?>
 
-                                    <h1>Most Similar Artists:</h1>
-                                    <?php
-                                            for ($k = 1; $k <= 10; $k++){
-                                                print "<h2>{$k}: result goes here Sprint #3!</h2>";
-                                            }
-                                    ?>
+                        </form>
 
-                            </center>
-                    </div>
-                    <!-- <p> just creates a new paragraph or more specifically a body of text. -->
+                </div>
 
-                    <div class="column" style="min-width: 400px; margin: 0 auto;">
+                <div class="column" style="min-width: 400px; margin: 0 auto;">
+                        <center>
+                                <?php
+                                if ($_SESSION["search"] == ""){
+                                        print "<h1>No results found.</h1>";
+                                }
+                                else{
 
-                            <form action = "statistics.php">
-                                    <center>
+                                        if ($_SESSION["username"] == ""){
+                                                print "<h1>We found: </h1> <h2>{$_SESSION["search"]}</h2>";
+                                        }
 
-                                            <!-- Using our special .button class to make the button look a precise way, plus also text centering.  -->
-                                            <button class="button button1"><b>Statistics</b></button><br>
+                                        else{
+                                                print "<h1>{$_SESSION["username"]}, we found: </h1> <h2>{$_SESSION["search"]}</h2>";
+                                        }
+                                }
+                                ?>
 
-                                    </center>
+                                <?php
+                                        $id = get_artist_id($_SESSION['search'], $_SESSION['artist_tracks']) or die("hi");
+                                        $url = get_artist_image($id[0], $_SESSION['token'])["url"];
+                                        if ($url != ""){
+                                                print "<img src='{$url}' width = '200' height = '200' style = 'border: 5px solid black;'></img>";
+                                                }
 
-                            </form>
-                    </div>
-            </div>
+                                        if ($_SESSION["search"] != ""){
 
-            <div><center><p>Wrong artist? Did you enter their name correctly?</p> </center></div>
-                <br><br><br>
+
+
+                                                print "<h1>Most Similar Artists:</h1>:";
+
+                                                for ($k = 1; $k <= 10; $k++){
+                                                        print "<h2>{$k}: result goes here Sprint #3!</h2>";
+                                                }
+                                        }
+                                ?>
+
+                        </center>
+                </div>
+                <!-- <p> just creates a new paragraph or more specifically a body of text. -->
+
+                <div class="column" style="min-width: 400px; margin: 0 auto;">
+
+                        <form action = "statistics.php">
+                                <center>
+
+                                        <!-- Using our special .button class to make the button look a precise way, plus also text centering.  -->
+                                        <button class="button button1"><b>Statistics</b></button><br>
+
+                                </center>
+
+                        </form>
+                </div>
+        </div>
+
+        <div><center><p>Wrong artist? Did you enter their name correctly?</p> </center></div>
+        <br><br><br>
 
 <?php include "footer.php"; ?>
 
