93454d5ad6ff91206b4e0aa27c1e46c4383ba481
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-03-21 23:52:34 -0400

    Task #122, limit user history
    This removes any entries in the 11 onward range.  It also does some string regex to account for weird errors from system memory.  For now, this only works on alphanumerics.

--- static/php/set_artist.php
+++ static/php/set_artist.php
@@ -10,12 +10,22 @@ include '../../spotify/get_songs.php';
 // Set the working artist.
 
 // A boolean check to make sure that the fields have values.
+
  if (isset($_POST['artist'])) {
 
-    $current_artist = $_POST['artist'] or die("I tried:(");
-    $current_artist = explode(":", $current_artist)[1];
+    if ($_POST['artist'] == "0"){
+       $current_artist = "0";
+    }
+    else{
+         $current_artist = $_POST['artist'] or die("I tried:(");
+    }
+
+    $current_artist = preg_replace("/[^a-z0-9]/i", "", $current_artist);
+    $temp_string = $current_artist[0];
+    $current_artist = preg_replace("/[0-9]+/", "", $temp_string) . substr($current_artist, -strlen($current_artist) + 1);
 
     $len = strlen(trim($current_artist));
+
     if ($len == 0){
         echo "<script>alert('You must have a valid search!');</script>";
         echo "<script> location.href = '../index.php'; </script>";
@@ -35,35 +45,39 @@ include '../../spotify/get_songs.php';
     $top_match = get_artist_id($current_artist, $tracks)[1];
 
     if ($top_match != ""){
-       $_SESSION['search'] = $top_match;
+    $_SESSION['search'] = $top_match;
 
-       $user = $_SESSION["username"];
-       if ($user != ""){
+    $user = $_SESSION["username"];
+    if ($user != ""){
 
-           // MAKE SURE THAT THE FILE HAS {} IN IT IF EMPTY, + CHMOD 777
-           $file = '../data/history.json';
-           $json = file_get_contents($file) or die('No Open!');
-           $dict = json_decode($json, true);
+        // MAKE SURE THAT THE FILE HAS {} IN IT IF EMPTY, + CHMOD 777
+        $file = '../data/history.json';
+        $json = file_get_contents($file) or die('No Open!');
+        $dict = json_decode($json, true);
 
-           // Does the user exists?
-           if (array_key_exists($user, $dict) == False){
-                   $dict[$user] = array();
-           }
+        // Does the user exists?
+        if (array_key_exists($user, $dict) == False){
+                $dict[$user] = array();
+        }
 
-           $array = $dict[$user];
-           $array[] = $top_match;
-           $dict[$user] = $array;
+        $array = $dict[$user];
+        $array[] = $top_match;
 
-           $new_json = json_encode($dict);
-           file_put_contents($file, $new_json);
-       }
+        while (count($array) > 10){
+                array_shift($array);
+        }
 
-       $_SESSION['token'] = $token;
-       $_SESSION['artist_tracks'] = $tracks;
-       echo "<script> location.href = '../result.php'; </script>";
-      }
+        $dict[$user] = $array;
+
+        $new_json = json_encode($dict);
+        file_put_contents($file, $new_json);
+    }
+
+    $_SESSION['token'] = $token;
+    $_SESSION['artist_tracks'] = $tracks;
+    echo "<script> location.href = '../result.php'; </script>";
+    }
     }
  }
 
 ?>
-</html>
