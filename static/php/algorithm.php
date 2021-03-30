ec965159a5742c6cc248f848f72df079357ed3d3
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-03-30 00:14:40 -0400

    Task #155, edge weights list
    I add the edge weights list for only the starting stack.  We can use this as a starting point and build from there.

--- static/php/algorithm.php
+++ static/php/algorithm.php
@@ -3,25 +3,45 @@ session_start();
 ?>
 <?php
 
-function get_stack(){
+       function get_stack(){
 
-    $all_tracks = $_SESSION['all_tracks'];
-    $input = $_SESSION['search'];
-    $artist_stack = array();
-    foreach($all_tracks as $track){
-        foreach($track['artists'] as $artist){
-                if (in_array($artist['name'], $artist_stack) == False and ($artist['name'] == $input) == False){
-                        $artist_stack[] = $artist['name'];
-                 }
+            $all_tracks = $_SESSION['all_tracks'];
+            $input = $_SESSION['search'];
+            $artist_stack = array();
+            $artist_weights = array();
+            foreach($all_tracks as $track){
+                foreach($track['artists'] as $artist){
+                        if (($artist['name'] == $input) == False){
+                             if (in_array($artist['name'], $artist_stack) == False){
+                                $artist_stack[] = $artist['name'];
+                               }
+                         }
+
+               }
+
+            }
+            return $artist_stack;
        }
 
-    }
-  
-    return $artist_stack;
-  
-}
+       function get_adjacency_list($all_tracks, $input){
+            $artist_stack = array();
+            $artist_weights = array();
+            foreach($all_tracks as $track){
+                foreach($track['artists'] as $artist){
+                        $artist_tuple = $input .  ':::' . $artist['name'];
+                        if (($artist['name'] != $input)){
 
-?>
+                             if (in_array($artist['name'], $artist_stack) == False){
+                                $artist_stack[] = $artist['name'];
+                                $artist_weights[$artist_tuple] = 0;
+                               }
 
+                             $artist_weights[$artist_tuple] += 1;
+                         }
+               }
+            }
+            arsort($artist_weights);
+            $artist_weights;
+        }
 
-
+ ?>
