f717af40c3ec9aeeebf62220a943dadfd236d12b
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-04-01 01:05:55 -0400

    Task #160, artist followers
    Here, I add an artist's number of followers.  It's normally in an integer, so we have to reconstruct it so that the commas are in the correct place.  This is easier to read for an everyday person.

--- static/statistics.php
+++ static/statistics.php
@@ -16,11 +16,25 @@ session_start();
             print "<h2>Artist Name: {$_SESSION['search']}</h2>"
           ?>
 
-   	 <h2>Monthly Listeners:</h2>
    	 <?php
                 $info = get_artist_info($_SESSION['artist_id'], $_SESSION['token']);
+
+                $followers = $info['followers']['total'];
+                $followers_array = array_reverse(str_split($followers));
+                $better_followers = "";
+                $i = 0;
+                foreach($followers_array as $char){
+                     if ($i % 3 == 0 and $i != 0){
+                         $better_followers = ',' . $better_followers;
+                     }
+                     $better_followers = $char . $better_followers;
+                     $i += 1;
+
+                }
+
+                print "<h2>Followers: {$better_followers}</h2>";
                 print "<h2>Genre: {$info['genres'][0]}</h2>";
-        ?>
+         ?>
   	</center>
    </div>
    
