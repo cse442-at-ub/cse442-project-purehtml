4bf0de68076d1faf62892c2e72d41a84d598dae6
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-04-02 01:33:03 -0400

    Task #152, get artist collaborations
    Here, I move a lot of the functions from statistics.php into here.

--- static/php/calculate_statistics.php
+++ static/php/calculate_statistics.php
@@ -4,7 +4,6 @@ session_start();
 
 <?php
 
-   $tracks = $_SESSION['artist_tracks'];
    function get_song_length($tracks){
        $lengths = array();
        foreach($tracks as $track){
@@ -13,5 +12,30 @@ session_start();
        }
        return $lengths;
    }
+
+   function get_followers($info){
+
+                $followers = $info['followers']['total'];
+                $followers_array = array_reverse(str_split($followers));
+                $better_followers = "";
+                $i = 0;
+                foreach($followers_array as $char){
+                     if ($i % 3 == 0 and $i != 0){
+                         $better_followers = ',' . $better_followers;
+                     }
+                     $better_followers = $char . $better_followers;
+                     $i += 1;
+               }
+        return $better_followers;
+    }
+
+    function get_collaborations($tracks){
+       $collabs = 0;
+       foreach($tracks as $track){
+           $artists = $track['artists'];
+           $collabs += (count($artists) - 1);
+       }
+       return $collabs;
+    }
 
 ?>
