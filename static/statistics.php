6827a8d4e98dc3f6352c47cdbc40897e61f9d792
Faizaan Arshad <marshad2@buffalo.edu>
2021-04-04 05:17:31 -0400

    Task #163, #164, Average song length on stats page

--- static/statistics.php
+++ static/statistics.php
@@ -6,9 +6,14 @@ session_start();
 <html>
 <?php include "header.php"; ?>
 <?php include "php/calculate_statistics.php"; ?>
+<?php include "../spotify/get_songs.php";?>
 
 <body style="background-color: #77d94c">
-   <div>
+   <div class="row">
+
+                <!-- <div> elements are stacked vertically so this will appear under the last <div>. -->
+
+   <div class="column" style="min-width: 400px; margin: 0 auto;">
   	<center>
    	 <h1>Artist Statistics</h1>
 
@@ -17,22 +22,41 @@ session_start();
           ?>
 
    	 <?php
-                $info = get_artist_info($_SESSION['artist_id'], $_SESSION['token']);
+              
+
+                $id_artist = get_artist_id($_SESSION['search'], $_SESSION['artist_tracks']);
+              
+                $info = get_artist_info($id_artist[0], $_SESSION['token']);
+                
+                
                 $followers = get_followers($info);
                 $collaborations = get_collaborations($_SESSION['artist_tracks']);
+                $results = get_song_length($_SESSION['artist_tracks']);
+                $number = average_length($results, $_SESSION['artist_tracks']);
+                
 
                 print "<h2>Total Followers: {$followers}</h2>";
                 print "<h2>Popularity: {$info['popularity']}/100</h2>";
                 print "<h2>Total Collaborations: {$collaborations}</h2>";
                 print "<h2>Genre: {$info['genres'][0]}</h2>";
+                print "<h2>Average Song Length: {$number}</h2>";
 
          ?>
   	</center>
    </div>
-   
+   <div class="column" style="min-width: 400px; margin: 0 auto;">
+                  <?php
+                  $id = get_artist_id($_SESSION['search'], $_SESSION['artist_tracks']) or die("hi");
+                  $url = get_artist_image($id[0], $_SESSION['token'])["url"];
+                  if ($url != ""){
+                    print "<img src='{$url}' width = '200' height = '200' style = 'border: 5px solid black;'></img>";
+                      }
+                  ?>
+                </div>
+              </div>
    <?php
-     $results = get_song_length($_SESSION['artist_tracks']);
-     echo var_dump($results);
+     //$results = get_song_length($_SESSION['artist_tracks']);
+     //echo var_dump($results);
    ?>
 <?php include "footer.php"; ?>
 </body>
