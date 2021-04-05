cb3338a9ca5caefd993e44f097979efc3f466e69
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-04-05 19:56:57 -0400

    Task #179, user page PHP
    I add the user page PHP code so that it can be accessed with the right URL.

---
+++ static/userpage.php
@@ -0,0 +1,21 @@
+<?php
+// Start the session
+session_start();
+?>
+
+<html>
+
+<?php include "header.php"; ?>
+
+<!-- The body.  Basically, where all of the content goes. -->
+<body style="background-color: #77d94c">
+     <div>
+          <center>
+          <h1>User Page Soon</h1>
+          </center>
+
+     </div>
+
+    <?php include "footer.php"; ?>
+</body>
+</html>
