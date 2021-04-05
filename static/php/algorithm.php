8d1e357bf831c16c0b1ef407dcc88ade57099525
k-z-m-r <45672136+k-z-m-r@users.noreply.github.com>
2021-04-05 01:15:33 -0400

    Task #175, first BFS layer
    I do the first layer of the BFS and return a weight list based on that.

--- static/php/algorithm.php
+++ static/php/algorithm.php
@@ -1,23 +1,29 @@
+
 <?php
 session_start();
 ?>
 <?php
 
+include "../../spotify/get_songs.php";
+
        function get_stack(){
 
             $all_tracks = $_SESSION['all_tracks'];
             $input = $_SESSION['search'];
-            $artist_stack = array();
+            $artist_stack = array($input);
             $artist_weights = array();
             foreach($all_tracks as $track){
+                $lead_artist = $track['artists'][0]['name'];
+                if ($lead_artist == $input){
                 foreach($track['artists'] as $artist){
                         if (($artist['name'] == $input) == False){
                              if (in_array($artist['name'], $artist_stack) == False){
-                                $artist_stack[] = $artist['name'];
+                                $artist_stack[] = $artist['id'];
                                }
                          }
 
                }
+             }
 
             }
             return $artist_stack;
@@ -27,6 +33,8 @@ session_start();
             $artist_stack = array();
             $artist_weights = array();
             foreach($all_tracks as $track){
+                $lead_artist = $track['artists'][0]['name'];
+                if ($lead_artist == $input){
                 foreach($track['artists'] as $artist){
                         $artist_tuple = $input .  ':::' . $artist['name'];
                         if (($artist['name'] != $input)){
@@ -39,9 +47,37 @@ session_start();
                              $artist_weights[$artist_tuple] += 1;
                          }
                }
+              }
             }
             arsort($artist_weights);
             return $artist_weights;
         }
+
+     function bfs($stack){
+        $popped = array($stack[0]);
+        array_shift($stack);
+        $token = $_SESSION['token'];
+        $weights = array();
+
+        while (sizeof($stack) > 0){
+            $head =  $stack[0];
+            $popped[] = $head;
+            array_shift($stack);
+
+            $info = get_artist_info($head, $token);
+            $tracks = get_all_tracks(get_all_albums($info['id'], $token), $token);
+            if (sizeof($tracks) > 0){
+            $new_list = get_adjacency_list($tracks, $info['name']);
+
+            $weights[$info['name']] = $new_list;
+            }
+
+
+
+        }
+
+       return $weights;
+    }
+
 
  ?>
