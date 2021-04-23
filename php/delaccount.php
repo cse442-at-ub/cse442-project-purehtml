0cc7ab758a2804895d3ad0d6873128d38f718db0
k-z-m-r <jdkazime@buffalo.edu>
2021-04-23 00:03:16 -0400

    Task #216, decouple user history
    On deleting account, I decouple user history if the user has history.

--- php/delaccount.php
+++ php/delaccount.php
@@ -17,6 +17,20 @@ $conn = connect('../db/db_credentials.txt');
     $stmt->bind_param("s", $_SESSION['username']);
     $stmt->execute();
 
+    $stmt = $conn->prepare("SELECT * FROM History WHERE username = ?");
+    $stmt->bind_param("s", $_SESSION['username']);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $qry = $result -> fetch_array(MYSQLI_NUM);
+    if (is_null($qry) == False){
+	$sql = "DELETE FROM History WHERE username = ?;";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $_SESSION['username']);
+        $stmt->execute(); 
+    }
+
+
     $user = $_SESSION['username'];
     $dir = '../data/profile_pics/';
     $loc = $dir . $user . '.png';
